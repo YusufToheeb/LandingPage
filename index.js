98b9968d289585000c0340b1a3e729bc5e3fc372
YusufToheeb <toheebyusuf26@gmail.com>
2022-06-25 22:49:36 +0100

    second commit

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+document.body.style.backgroundImage = "Coca002.jpg";
+
+
 function imgSlider(anything) {
     document.querySelector('.Coca').src = anything;
 }
@@ -7,3 +10,7 @@ function changeBgColor(color) {
     sec.style.background = color;
 
 }
+
+document.querySelector('h2').innerHTML = '<em>Life</em>'
+document.querySelector('p').innerHTML = '<strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, dolores! Nulla suscipit cumque repellat voluptatibus similique minus obcaecati, soluta laborum magnam facere nemo dolor quasi cum architecto debitis iure veniam.</strong>'
+document.querySelectorAll('a')[6].innerHTML = 'Products available'
